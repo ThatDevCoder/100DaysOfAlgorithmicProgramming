46412675e558a21a0ad7e914c8cddfe547416287
Shubham Gupta <shubham15gupta09@gmail.com>
2020-10-01 11:58:57 +0530

    Update 2_sum.java

--- 2_sum.java
+++ 2_sum.java
@@ -1,10 +1,8 @@
 /*
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
         Example:
         Given nums = [2, 7, 11, 15], target = 9,
-
         Because nums[0] + nums[1] = 2 + 7 = 9,
         return [0, 1].
  */
@@ -14,7 +12,7 @@ You may assume that each input would have exactly one solution, and you may not 
   
   
   Second approach
-  Using Hasmaps as these have linear time complexity of O(1) and we have to loop through only for loop that is O(n)
+  Using HasMaps as these have linear time complexity of O(1) and we have to loop through only for loop that is O(n)
   Therefore the time complexity is O(n)
   
 Code:
